4e34849d594305ffec7e29a07943fbc4296d1f2a
manu <manuelacostafaerman@gmail.com>
2024-10-12 14:57:42 -0300

    changing  ModuleNotFoundError: No module named 'HMS.settings.local'

--- HMS/settings/settings_production.py
+++ HMS/settings/settings_production.py
@@ -1,8 +1,8 @@
 import dj_database_url
 from decouple import config
-
 DEBUG: False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
 
 DATABASES = {
     'default': dj_database_url.config(
