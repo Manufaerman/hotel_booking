d270c764fed031e0ed9694f8733619820e02a31d
manu <manuelacostafaerman@gmail.com>
2024-10-13 14:45:46 -0300

    now the app is showing messages when db is empty

--- HMS/settings/settings_production.py
+++ HMS/settings/settings_production.py
@@ -1,7 +1,7 @@
 import dj_database_url
 from decouple import config
 DEBUG: False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'powerful-garden-90023-0b97dae15c35.herokuapp.com', 'herokuapp.com']
 
 
 DATABASES = {
