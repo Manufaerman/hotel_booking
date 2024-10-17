54a659d4a9369a85a6bd6f8b1df8d333c4b4f723
manu <manuelacostafaerman@gmail.com>
2024-10-17 08:30:34 -0300

    change in signal userprfile

--- user_profile/models.py
+++ user_profile/models.py
@@ -21,5 +21,6 @@ class UserProfile(models.Model):
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
+        print(kwargs)
         instance.userprofile.save()
 
