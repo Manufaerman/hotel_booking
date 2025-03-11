674d5bca24bcc8bf5fd25357077e91d160387a18
manu <manuelacostafaerman@gmail.com>
2025-03-11 11:13:29 +0100

    VPS

--- HMS/settings/production.py
+++ HMS/settings/production.py
@@ -1,9 +1,11 @@
 import dj_database_url
 from decouple import config
 from pathlib import Path
+from .base import *
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-ALLOWED_HOSTS = ['*', 'www.smartagency.es', 'smartagency.es']
+ALLOWED_HOSTS = ['*', 'www.smartagency.es', 'smartagency.es', '46.202.175.212']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -19,7 +21,7 @@ SECURE_SSL_REDIRECT = False
 DEBUG = False
 
 STATICFILES_STORAGE = 'django.contrib.static.storage.StaticFilesStorage'
-ALLOWED_HOSTS = ['powerful-garden-90023-0b97dae15c35.herokuapp.com', 'localhost', '127.0.0.1', 'https://powerful'
+ALLOWED_HOSTS = ['', 'localhost', '127.0.0.1', ''
                                                                                                '-garden-90023'
                                                                                                '-0b97dae15c35'
-                                                                                               '.herokuapp.com', '*', ]
+                                                                                               '.herokuapp.com', '*','46.202.175.212']
