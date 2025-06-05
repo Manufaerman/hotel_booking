def73c7824ed01702765b1f58c6c62357a9022a8
manu <manuelacostafaerman@gmail.com>
2025-06-05 21:18:38 +0200

    new features

--- HMS/settings/production.py
+++ HMS/settings/production.py
@@ -3,6 +3,22 @@ from decouple import config
 from pathlib import Path
 from .base import *
 
+import sys
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOWED_HOSTS = ['*', 'www.smartagency.es', 'smartagency.es', '46.202.175.212', 'localhost']
