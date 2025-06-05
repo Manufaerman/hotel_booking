c40d1fa78c0fe54f106e0aa3712041e1418d73c3
manu <manuelacostafaerman@gmail.com>
2025-06-05 16:30:19 +0200

    new features

--- hotel/urls.py
+++ hotel/urls.py
@@ -10,29 +10,34 @@ from .views import Home, BookRoomClient, \
 app_name = 'hotel'
 
 urlpatterns = [
+    # Esta es la ruta principal, sin parámetros
+    path('', home.as_view(), name='home'),
+
 
     path('kakashka/<str:id>', roomandflats, name='roomandflats'),
     path('room_list/<id>', home.as_view(), name='roomlist'),
 
 
-    path('dashboard/la/la/la/', DashboardBookMonth.as_view(), name='dashboardbookmonth'), #new
-    path('dashboard/la/la/la/<id>', DashboardBookMonth.as_view(), name='dashboardbookmonth'), #new
-    path('dashboard/la/la/la/<id>/<month>', DashboardBookMonth.as_view(), name='dashboardbookmonth'),
-    path('reserva/eliminar/<int:pk>/', views.eliminar_reserva, name='eliminar'),
+    path('/dashboard/la/la/la/', DashboardBookMonth.as_view(), name='dashboardbookmonth'), #new
+    path('/dashboard/la/la/la/<id>', DashboardBookMonth.as_view(), name='dashboardbookmonth'), #new
+    path('/dashboard/la/la/la/<id>/<month>', DashboardBookMonth.as_view(), name='dashboardbookmonth'),
+
+
+    path('/reserva/eliminar/<int:pk>/', views.eliminar_reserva, name='eliminar'),
     #using it to display charts
-    path('dashboard/', dashboard, name='dashboard'),
-    path('api/data/', get_data, name='api_data'),
-    path('api/chart/data/', ChartData.as_view()),
-    path('biografia/', biografia, name='biografia'),
+    path('/dashboard/', dashboard, name='dashboard'),
+    path('/api/data/', get_data, name='api_data'),
+    path('/api/chart/data/', ChartData.as_view()),
+    path('/biografia/', biografia, name='biografia'),
 
     #using jquery to show models in template
-    path('pruebas/', kakashka, name='pruebas'),
-    path('pruebas-json/', Modify.as_view(), name='pruebas-json'),
+    path('/pruebas/', kakashka, name='pruebas'),
+    path('/pruebas-json/', Modify.as_view(), name='pruebas-json'),
 
-    path('booking_list/', Home.as_view(), name='bookinglist'),
+    path('/booking_list/', Home.as_view(), name='bookinglist'),
     path('room/<category>', BookRoomClient.as_view(), name='roomdetailview'),
-    path('booking/cancel/<pk>', CancelBookingView.as_view(), name='cancelbookingview'),
-    path('', home.as_view(), name='home'),
+    path('/booking/cancel/<pk>', CancelBookingView.as_view(), name='cancelbookingview'),
+
     path('reservas/', AllBookings.as_view(), name='reservas'),
     path('reservas/editar/<int:pk>', BookingUpdateView.as_view(), name='editar')
 
